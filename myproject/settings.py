35d569c3901d559afedcc0f6f113bbe120bf41ed
Vonn Adrian <vonnadrianjutar123@gmail.com>
2025-02-04 10:01:49 +0800

    Block admin access fix

--- myproject/settings.py
+++ myproject/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'myapp.middleware.BlockAdminAccessMiddleware',  # Block admin access
 ]
 
 ROOT_URLCONF = 'myproject.urls'
@@ -79,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'agri_db',  # Replace with your database name
         'USER': 'root',               # Default MySQL username
-        'PASSWORD': '',  # Replace with your MySQL password
+        'PASSWORD': 'root',  # Replace with your MySQL password
         'HOST': '127.0.0.1',          # Default MySQL host
         'PORT': '3306',
     }
