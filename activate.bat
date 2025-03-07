24efe530d7958cd21a991e36c16fffc95a40e82e
Vonn Adrian <vonnadrianjutar123@gmail.com>
2025-03-07 10:12:52 +0800

    Update activate.bat: enhance script for easier server startup with SSL configuration and clearer instructions

--- activate.bat
+++ activate.bat
@@ -1,8 +1,10 @@
+:: This bat script is intended to make running the server easier
+:: You still need to do the things in README.MD
+:: You also need to edit the things here such as the IP address, cd path, etc.
 @echo off
-cd "C:\Users\admin\Desktop\Time Keeping System\Time_Keeping_System"
-call .\venv\Scripts\activate.bat
-start python manage.py runserver 0.0.0.0:8000 --cert-file cert.pem --key-file key.pem
+cd "C:\Path\To\The\System"
+start python manage.py runsslserver IP_ADDRESS_HERE:8000 --certificate CERT_NAME.pem --key CERT_KEY_NAME.pem
 
-    
+
 :: Please wait for the server to start
 timeout /t 5
