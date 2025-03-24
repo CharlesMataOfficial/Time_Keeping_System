f2bc5acb5e2a80139e8c014098a5e3c1dd5c0d49
CharlesMataOfficial <161152355+CharlesMataOfficial@users.noreply.github.com>
2025-03-24 17:58:36 +0800

    remove root password in settings

--- agridom_attendance/settings.py
+++ agridom_attendance/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "agri_db", # This is the database the django app uses
         "USER": "root",
-        "PASSWORD": "root",
+        "PASSWORD": "",
         "HOST": "127.0.0.1",
         "PORT": "3306",
     },
